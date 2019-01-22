1d4ec210f3cf8628e7ba8b2553ca3127017da660
libo <1293325947@qq.com>
2019-01-22 17:07:04 +0800

    banner页面

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const path=require('path');
+const cookieParser=require('cookie-parser');
+const app=express();
+const indexRouter=require('./routes/index');
+const bannerRouter=require('./routes/banner');
+app.use(express.static(path.resolve(__dirname,'./public')));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set('views',path.resolve(__dirname,'./views'));
+app.set('view engine','ejs');
+app.use('/',indexRouter);
+app.use('/banner',bannerRouter);
+app.listen(3000);
